c1852ff1c0473f71e9558ae554545ad7f6555560
Syed Faruque <talhafaruque6@gmail.com>
2024-07-23 03:31:12 -0400

    Update useHandCardZIndex.jsx

--- frontend/src/hooks/useHandCardZIndex.jsx
+++ frontend/src/hooks/useHandCardZIndex.jsx
@@ -3,6 +3,10 @@
  * created: July 7 2024
 **/
 
+// to keep the illusion that a card that is pulled is underneath the ones on its right and over the ones on its left
+// the z index of all cards in the hand are mutated during an animation
+// the implementation of this can be found in hands component
+
 const useHandCardZIndex = (index, animationFromHand) => {
     if (!animationFromHand.active) return 1;
     if (index < animationFromHand.card_index) return 0;
