2d6cccae4b42e51795e205fbcb30f51c6e9c3669
Syed Faruque <talhafaruque6@gmail.com>
2024-07-23 03:29:03 -0400

    Update useAnimationToHandStartingPoint.jsx

--- frontend/src/hooks/useAnimationToHandStartingPoint.jsx
+++ frontend/src/hooks/useAnimationToHandStartingPoint.jsx
@@ -1,6 +1,14 @@
+/**
+ * @author Syed Faruque
+ * created: July 7 2024
+**/
+
 import { useState } from "react";
 import getDeckPosition from "../helpers/getDeckPosition";
 import useWindowSize from "./useWindowSize";
+
+// the starting position of a card animating to the hand should change based on whether it was pulled from the deck or pile.
+// this coordinates and passes on the state of the initial position of a animation to the hand.
 
 const useAnimationToHandStartingPoint = (deck) => {
 
