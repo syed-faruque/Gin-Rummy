d73b67ff344f80aea3ec0d1ac73b3a7af19b2dfd
Syed Faruque <talhafaruque6@gmail.com>
2024-08-03 22:59:39 -0400

    Add files via upload

---
+++ frontend/src/hooks/useKnockData.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+
+const useKnockData = (userCollection, hands) => {
+    const [knockData, setKnockData] = useState({
+        ready: false,
+        deadwood_total: 0,
+        index_max_value_card: null
+    });
+
+    useEffect(() => {
+        const user_hand = hands.user_hand || [];
+        const deadwood = userCollection.user_deadwood || [];
+
+        const deadwood_total = deadwood.reduce((total, card) => 
+            (card.value === 11 || card.value === 12 || card.value === 13) // the value of Q, K, and J are 10 even though I set their properties are different
+            ? total + 10 
+            : total + card.value, 0
+        );
+
+        let max_value_card = null;
+        let index_card = null;
+        let max_value = 0;
+
+        for (let i = 0; i < deadwood.length; i++) {
+            const card = deadwood[i];
+            if (card.value > max_value) {
+                max_value = card.value;
+                max_value_card = card;
+                index_card = user_hand.findIndex(userCard => userCard.src === card.src);
+            }
+        }
+
+        const knockReady = (deadwood_total - max_value) <= 10;
+
+        setKnockData({
+            ready: knockReady,
+            deadwood_total,
+            index_max_value_card: index_card
+        });
+
+    }, [userCollection]);
+
+    return knockData;
+}
+
+export default useKnockData;
