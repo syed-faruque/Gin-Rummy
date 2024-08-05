60cce8e5fffe998c8d250d768e62771d276d837d
Syed Faruque <talhafaruque6@gmail.com>
2024-08-04 23:11:44 -0400

    update to allow for knock functionality

--- frontend/src/components/Pile.jsx
+++ frontend/src/components/Pile.jsx
@@ -16,8 +16,11 @@ const Pile = ({
     deckInitialized, updateDeckInitialized,
     pileInitialized, updatePileInitialized,
     handsInitialized, updateHandsInitialized,
+    animationToHandStartingPoint, updateAnimationToHandStartingPoint,
     animationToPileStartingPoint, updateAnimationToPileStartingPoint,
     animationFromHand, updateAnimationFromHand,
+    userKnocked, updateUserKnocked,
+    opponentKnocked, updateOpponentKnocked,
     turn, updateTurn,
     stage, updateStage,
     last, updateLast,
@@ -25,7 +28,7 @@ const Pile = ({
 }) => {
 
     useInitializePile(pile, pileInitialized, updatePile, updatePileInitialized, deck, updateDeck, deckInitialized, hands, handsInitialized);
-    usePileListeners(socket, windowSize, updateAnimationFromHand, updateAnimationToPileStartingPoint, updateLast, hands, updateHands, handsInitialized, deck, deckInitialized, pile, updatePile, pileInitialized);
+    usePileListeners(socket, windowSize, updateAnimationFromHand, updateAnimationToPileStartingPoint, updateLast, hands, updateHands, handsInitialized, deck, deckInitialized, pile, updatePile, pileInitialized, updateUserKnocked, updateOpponentKnocked);
 
     const emitDrawNotice = (turn, stage) => {
         if (turn && (stage == 0 || stage == 1)) {
@@ -48,7 +51,11 @@ const Pile = ({
 
     // after animation from the hand to the pile. The animation.active needs to be set to false, everything else default values
                     callBack = {() => {
-                        if (pileInitialized && stage == 2 && animationFromHand.active) {
+                        if ((userKnocked || opponentKnocked) && animationFromHand.active) {
+                            updateStage(3);
+                            updateAnimationFromHand({...animationFromHand, card_index: null, user: false, active: false})
+                        }
+                        else if (pileInitialized && stage == 2 && animationFromHand.active) {
                             updateTurn();
                             updateStage(1);
                             updateAnimationFromHand({...animationFromHand, card_index: null, user: false, active: false})
