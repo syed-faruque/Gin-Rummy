d05f5302e539300e0944b61b2e4c732d8e889889
Syed Faruque <talhafaruque6@gmail.com>
2024-09-01 21:21:47 -0400

    fixed condition

--- frontend/src/helpers/getAnimationToHandFinalPoint.jsx
+++ frontend/src/helpers/getAnimationToHandFinalPoint.jsx
@@ -5,18 +5,19 @@ import getSortedHandPosition from "./getSortedHandPosition";
 const getAnimationToHandFinalPoint = (animationFromHand, windowSize, user_hand_length, opponent_hand_length, index, stage, userKnocked, opponentKnocked, sortedHandIndexes, userCollection, opponentCollection) => {
 
     const {user_sorted_hand_indexes, opponent_sorted_hand_indexes} = sortedHandIndexes;
-
     const userStandardHandPosition = getHandPosition(index, true, user_hand_length, windowSize);
     const opponentStandardHandPosition = getHandPosition(index, false, opponent_hand_length, windowSize);
     const userHandPositionBeforeDraw = getHandPositionBeforeDraw(index, animationFromHand, windowSize);
     const opponentHandPositionBeforeDraw = getHandPositionBeforeDraw(index, animationFromHand, windowSize);
 
-    if ((userKnocked || opponentKnocked) && !animationFromHand.active) {
+    if ((userKnocked || opponentKnocked) && stage >= 3) {
+        
         return {
             userAnimationToHandFinalPoint: getSortedHandPosition(user_sorted_hand_indexes.indexOf(index), true, userCollection, windowSize),
             opponentAnimationToHandFinalPoint: getSortedHandPosition(opponent_sorted_hand_indexes.indexOf(index), false, opponentCollection, windowSize)
         }
     }
+    
     else if (animationFromHand.active && animationFromHand.user) {
         return {userAnimationToHandFinalPoint: userHandPositionBeforeDraw, opponentAnimationToHandFinalPoint: opponentStandardHandPosition}
     }
